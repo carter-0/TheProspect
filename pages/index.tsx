5e32cfaf8492d0a66541ec34702a3ab662c03d9d
carter-0 <carterannandale@gmail.com>
2023-08-14 22:15:27 +0100

    feat: add live music section.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,86 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Head from "next/head";
+import Navbar from "@/components/Navbar";
+import LiveTable from "@/components/LiveTable";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <>
+            <Head>
+                <title>The Prospect</title>
+                <meta name="description" content="The Prospect" />
+                <link rel="icon" href="/favicon.ico" />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                <meta property="og:title" content="The Prospect" />
+                <meta property="og:description" content="Watch The Prospect's Performances" />
+                <meta property="og:image" content="https://api.theprospect.band/v1/performance_images/bricket-wood-social-club.webp" />
+                <meta property="og:url" content="https://theprospect.band/" />
+            </Head>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <Navbar />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            <main className={"bg-gradient-to-r from-[#b3b19c] to-[#dbd1ad]"}>
+                <header className="w-full h-[500px] md:h-[976px] bg-[url('/assets/main-background.webp')] bg-cover bg-center flex justify-center items-center">
+                    <div className="flex flex-col justify-center items-center pb-[200px] md:pb-[500px] mmd:pb-[400px] mmd:mt-10">
+                        <h1 className={"text-center md:text-9xl text-7xl text-black drop-shadow-lg font-bold prospect-font"}>The Prospect</h1>
+                    </div>
+                </header>
+                <div className={"flex flex-row py-40 justify-center items-center"}>
+                    <div className="max-w-7xl w-full">
+                        <h2 className={"text-6xl font-bold prospect-font text-center mb-10"}>Live Music</h2>
+                        <table className={"min-w-full hidden sm:table px-5 border-collapse"}>
+                            <tbody>
+                                <tr className={"border-b border-gray-800 sm:flex-row"}>
+                                    <td className={"font-bold py-5 text-xl"}>12 NOV 22</td>
+                                    <td className={"font-semibold"}>Bricket Wood</td>
+                                    <td className={"font-semibold"}>Bricket Wood Social Club</td>
+                                    <td className={"font-semibold cursor-not-allowed"}><s>Buy Tickets</s></td>
+                                </tr>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+                                <tr className={"border-b py-5 px-2 border-gray-800 sm:flex-row"}>
+                                    <td className={"font-bold py-5 text-xl"}>10 AUG 23</td>
+                                    <td className={"font-semibold"}>St Albans</td>
+                                    <td className={"font-semibold"}>The Horn</td>
+                                    <td className={"font-semibold cursor-not-allowed"}><s>Buy Tickets</s></td>
+                                </tr>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                                <tr className={"py-5 px-2 sm:flex-row"}>
+                                    <td className={"font-bold py-5 text-xl"}>15 AUG 23</td>
+                                    <td className={"font-semibold"}>Bricket Wood</td>
+                                    <td className={"font-semibold"}>Bricket Wood Social Club</td>
+                                    <td className={"font-semibold"}><Link target={"_blank"} href={"https://linktr.ee/the_prospect"}>Buy Tickets</Link></td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <div className={"flex flex-col items-center w-full sm:hidden"}>
+                            <div className={"border-b w-[300px] py-5 px-2 border-gray-800 sm:flex-row"}>
+                                <h3 className={"font-bold py-5 text-xl"}>12 NOV 22</h3>
+                                <h3 className={"font-semibold"}>Bricket Wood</h3>
+                                <h3 className={"font-semibold"}>Bricket Wood Social Club</h3>
+                                <h3 className={"font-semibold cursor-not-allowed"}><s>Buy Tickets</s></h3>
+                            </div>
+
+                            <div className={"border-b w-[300px] py-5 px-2 border-gray-800 sm:flex-row"}>
+                                <h3 className={"font-bold py-5 text-xl"}>10 AUG 23</h3>
+                                <h3 className={"font-semibold"}>St Albans</h3>
+                                <h3 className={"font-semibold"}>The Horn</h3>
+                                <h3 className={"font-semibold cursor-not-allowed"}><s>Buy Tickets</s></h3>
+                            </div>
+
+                            <div className={"py-5 w-[300px] px-2 sm:flex-row"}>
+                                <h3 className={"font-bold py-5 text-xl"}>15 AUG 23</h3>
+                                <h3 className={"font-semibold"}>Bricket Wood</h3>
+                                <h3 className={"font-semibold"}>Bricket Wood Social Club</h3>
+                                <h3 className={"font-semibold"}><Link target={"_blank"} href={"https://linktr.ee/the_prospect"}>Buy Tickets</Link></h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
 }
